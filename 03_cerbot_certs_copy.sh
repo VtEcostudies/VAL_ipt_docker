606e5a55eede653e55c7a558fce12c7baf897687
jloomisVCE <jloomis@vtecostudies.org>
2023-11-13 15:06:23 +0000

    add ls to check certs copy success

--- 03_cerbot_certs_copy.sh
+++ 03_cerbot_certs_copy.sh
@@ -1,12 +1,17 @@
-# Simply copy from letsencrypt symlink cert location, eg. 
+set -x
+# Simply copy from letsencrypt symlink cert location, eg.
 #  fullchain.pem -> ../../archive/ipt.vtatlasoflife.org/fullchain5.pem
 # which changes on each certs update, to a local file in the same directory.
-echo sudo cp /etc/letsencrypt/live/ipt.vtatlasoflife.org/fullchain.pem /etc/letsencrypt/live/ipt.vtatlasoflife.org/ipt.crt
-echo sudo cp /etc/letsencrypt/live/ipt.vtatlasoflife.org/privkey.pem /etc/letsencrypt/live/ipt.vtatlasoflife.org/ipt.key
+sudo cp -v /etc/letsencrypt/live/ipt.vtatlasoflife.org/fullchain.pem /etc/letsencrypt/live/ipt.vtatlasoflife.org/ipt.crt
+sudo cp -v /etc/letsencrypt/live/ipt.vtatlasoflife.org/privkey.pem /etc/letsencrypt/live/ipt.vtatlasoflife.org/ipt.key
 
 # NOTE: it does not appear that we need separate certs for vtecostudies.org,
 # since in docker-compose service:ipt CERT_NAME is 'ipt' and service:nginx DEFAULT_HOST
 # is 'ipt.vtatlasoflife.org'. However, if we do, then do this:
 
-echo sudo cp /etc/letsencrypt/live/ipt.vtecostudies.org/fullchain.pem /etc/letsencrypt/live/ipt.vtecostudies.org/eco.crt
-echo sudo cp /etc/letsencrypt/live/ipt.vtecostudies.org/privkey.pem /etc/letsencrypt/live/ipt.vtecostudies.org/eco.key
+sudo cp -v /etc/letsencrypt/live/ipt.vtecostudies.org/fullchain.pem /etc/letsencrypt/live/ipt.vtecostudies.org/eco.crt
+sudo cp -v /etc/letsencrypt/live/ipt.vtecostudies.org/privkey.pem /etc/letsencrypt/live/ipt.vtecostudies.org/eco.key
+
+echo "NOTE: DATES BELOW MUST BE NOW(). IF NOT THERE WAS A FAILURE."
+ls -alh /etc/letsencrypt/live/ipt.vtatlasoflife.org/ipt.*
+ls -alh /etc/letsencrypt/live/ipt.vtecostudies.org/eco.*
