404b42d8c6e804f6a760eee46e352ae925390d83
jloomisVCE <jloomis@vtecostudies.org>
2026-08-26 12:57:58 -0400

    AWS SES DNS records to enable sending domain vtatlasoflifefrom server.

---
+++ SES_test_DNS_CNAME_records.sh
@@ -0,0 +1,3 @@
+dig +short CNAME yiarcr7d4lcyr5c7dxbh6ujsqbheazsu._domainkey.vtatlasoflife.org
+dig +short CNAME ds45xrmqdegpkbrjb5nua7evubqipssc._domainkey.vtatlasoflife.org
+dig +short CNAME bmuvyi7hp6t3xg73v6rg2izb5z24hgoz._domainkey.vtatlasoflife.org
