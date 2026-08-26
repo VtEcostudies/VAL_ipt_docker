235ec2d5200299b518d42bd6a6dc91921fb89c75
jloomisVCE <jloomis@vtecostudies.org>
2026-08-26 15:21:56 +0000

    save ses-setup file for posterity

---
+++ ses-setup.sh
@@ -0,0 +1,5 @@
+./09_install_postfix.sh --apply \
+    --relay email-smtp.us-east-1.amazonaws.com:587 \
+    --user AKIA377XAEZJWOPQPUNW \
+    --from jloomis@vtecostudies.org
+#    --from ipt@vtatlasoflife.org
